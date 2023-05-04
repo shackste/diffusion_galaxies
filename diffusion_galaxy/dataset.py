2910ee87bbb091217de9c9390a84cf2245ea9bb3
Shackste <shackst3@gmail.com>
2023-05-04 16:46:33 +0200

    divide: either transform or custom loader (both transform ToTensor)

--- diffusion_galaxy/dataset.py
+++ diffusion_galaxy/dataset.py
@@ -6,13 +6,16 @@ import torch
 def create_pytorch_dataset(path_to_images, image_size, custom_loader=None):
     """Create a PyTorch dataset from a directory of images. The images are cropped equally on each side to be square, corresponding to the specified image size. """
     transform = transforms.Compose([
-#        transforms.CenterCrop(image_size),
-#        transforms.ToTensor(),
+        transforms.CenterCrop(image_size),
+        transforms.ToTensor(),
         transforms.Grayscale(),
         transforms.Normalize(mean=[0.5], std=[0.5])
 
     ])
-    dataset = datasets.ImageFolder(path_to_images, transform, loader=custom_loader)
+    if custom_loader:
+        dataset = datasets.ImageFolder(path_to_images, loader=custom_loader)
+    else:
+        dataset = datasets.ImageFolder(path_to_images, transform)
     return dataset
 
 
